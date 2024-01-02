5dd8a56f90f35c1f174d1546fcc09ac1ced84dbd
백상휘 <sanghwiback@gmail.com>
2024-01-02 15:31:53 +0900

    Add ui elements in storyboard
    - xib 추가
    - 라벨, 텍스트필드, 텍스트뷰, 테이블뷰, 이미지뷰를 넣음
    - 스토리보드 수정 없이 Objective-C 코드만 작업을 진행할 예정

---
+++ OBJC-Todo/MainTableViewCellView.h
@@ -0,0 +1,16 @@
+//
+//  MainTableViewCell.h
+//  OBJC-Todo
+//
+//  Created by 백상휘 on 2024/01/02.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainTableViewCellView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OBJC-Todo/MainTableViewCellView.m
@@ -0,0 +1,17 @@
+//
+//  MainTableViewCell.m
+//  OBJC-Todo
+//
+//  Created by 백상휘 on 2024/01/02.
+//
+
+#import "MainTableViewCellView.h"
+
+@implementation MainTableViewCellView
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
