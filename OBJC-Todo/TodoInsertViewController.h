2d7ef7662d7dcb8e1f6fc5d12c1f57930d32cb28
백상휘 <sanghwiback@gmail.com>
2024-01-13 11:16:11 +0900

    Add inputViewController delegate methods

---
+++ OBJC-Todo/TodoInsertViewController.h
@@ -0,0 +1,20 @@
+//
+//  TodoInsertViewController.h
+//  OBJC-Todo
+//
+//  Created by 백상휘 on 2024/01/13.
+//
+
+#import <UIKit/UIKit.h>
+#import "MainTableViewCell.h"
+
+@interface TodoInsertViewController : UIViewController<UITextFieldDelegate, UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextField *titleField;
+@property (weak, nonatomic) IBOutlet UITextView *contentsTextView;
+@property (weak, nonatomic) IBOutlet UIView *previewCell;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSubmit;
+@property int lengthLimit;
+
+@end
---
+++ OBJC-Todo/TodoInsertViewController.m
@@ -0,0 +1,104 @@
+//
+//  TodoInsertViewController.m
+//  OBJC-Todo
+//
+//  Created by 백상휘 on 2024/01/13.
+//
+
+#import "TodoInsertViewController.h"
+
+@implementation TodoInsertViewController
+
+@synthesize imageView;
+@synthesize titleField;
+@synthesize contentsTextView;
+@synthesize previewCell;
+@synthesize buttonSubmit;
+@synthesize lengthLimit;
+
+- (void)loadView {
+  [super loadView];
+  
+  UINib *nib = [UINib nibWithNibName:@"MainTableViewCell" bundle:nil];
+  UIView *first = [[nib instantiateWithOwner:@"MainTableViewCell" options:nil] firstObject];
+  [previewCell addSubview:first];
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  [titleField setDelegate:self];
+  [contentsTextView setDelegate:self];
+  lengthLimit = 15;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender { }
+
+// MARK: - UITextFieldDelegate
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+  
+  [self deleteLengthLimit:textField
+              currentText:[textField text]
+              changeRange:range
+          replacementText:string];
+  
+  [[self getPreviewCell].titleLabel setText:textField.text];
+  return TRUE;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+  [textField resignFirstResponder];
+  [contentsTextView becomeFirstResponder];
+  return TRUE;
+}
+
+// MARK: - UITextViewDelegate
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+  
+  [self deleteLengthLimit:textView
+              currentText:[textView text]
+              changeRange:range
+          replacementText:text];
+  
+  [[self getPreviewCell].contentsLabel setText:textView.text];
+  return TRUE;
+}
+
+// MARK: - Action Else
+
+- (IBAction)buttonSubmitTouchUpInsde:(UIButton *)sender {
+  [[self navigationController] popViewControllerAnimated:TRUE];
+}
+
+- (void)deleteLengthLimit:(id <UITextInput>)elem
+              currentText:(NSString *)currentText
+              changeRange:(NSRange)range
+          replacementText:(NSString *)text  {
+  NSUInteger ln = range.length;
+  
+  if ([text isEqual:@""] && ln > 0) { // When Delete
+    return;
+  }
+  
+  if ((ln + [text length] + [currentText length]) > lengthLimit) {
+    UITextPosition *s = [elem beginningOfDocument];
+    UITextPosition *leading = [elem positionFromPosition:s
+                                                  offset:(lengthLimit)];
+    UITextPosition *trailing = [elem positionFromPosition:s
+                                                   offset:(lengthLimit+ln)];
+    UITextRange *r = [elem textRangeFromPosition:leading
+                                      toPosition:trailing];
+    
+    [elem replaceRange:r withText:@""];
+    [elem setSelectedTextRange:[elem textRangeFromPosition:elem.endOfDocument
+                                                toPosition:elem.endOfDocument]];
+  }
+}
+
+- (MainTableViewCell *)getPreviewCell {
+  return [[previewCell subviews] firstObject];
+}
+
+@end
